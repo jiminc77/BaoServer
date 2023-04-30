ced57cb1f5e4c7f595f87e83eb50808e0915ab57
Ubuntu <ubuntu@ip-172-31-5-116.eu-north-1.compute.internal>
2023-04-30 04:03:10 +0000

    inference

--- app.py
+++ app.py
@@ -3,14 +3,20 @@ from detect import run
 
 app = Flask(__name__)
 
+@app.route('/', methods = ['GET'])
+def text():
+    return "Hello Flutter"
 
 @app.route('/predict', methods = ['POST'])
 def predict():
     if request.method == "POST":
         file = request.files['file']
         file.save("test_4.jpg")
+        
+        data = run()
+        print(data)
 
-        return jsonify({'test' : run()})
+        return jsonify({'test' : data})
 
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0', port = 5000, debug = True)
--- detect.py
+++ detect.py
@@ -59,7 +59,7 @@ def run(
         source='test_4.jpg',  # file/dir/URL/glob/screen/0(webcam)
         data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
         imgsz=(640, 640),  # inference size (height, width)
-        conf_thres=0.5,  # confidence threshold
+        conf_thres=0.3,  # confidence threshold
         iou_thres=0.5,  # NMS IOU threshold
         max_det=1000,  # maximum detections per image
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
